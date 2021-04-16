afabaeba685ddebae45ab23b22e4d20d9886c8ff
Jamie Phelps <james.phelps1995@live.com>
2021-04-16 19:00:06 +0100

    ssl confirmation

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
-const isProduction = process.env.NODE_ENV === 'production';
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
+  connectionString: process.env.DATABASE_URL || connectionString,
+  ssl: process.env.DATABASE_URL ? true : false,
 });
 
 module.exports = { pool };
+// postgres://wvsrhrxlowbnkc:02021bb22dace62e1a9b54ab1670edbff8b36767065250e94746be62362ec89f@ec2-34-225-103-117.compute-1.amazonaws.com:5432/d99jvgaoos1sq9
