f04f467a961781cffc5f94fccafa1610178aba00
Jamie Phelps <james.phelps1995@live.com>
2021-04-20 12:04:49 +0100

    connect direct to pg heroku database

--- db.js
+++ db.js
@@ -4,8 +4,10 @@ const { Pool } = require('pg');
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || connectionString,
-  ssl: process.env.DATABASE_URL ? true : false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = { pool };
