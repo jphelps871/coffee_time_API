0b61334a567694efc78585eb3c80a7ce246e3dda
Jamie Phelps <james.phelps1995@live.com>
2021-04-20 11:47:14 +0100

    add pg env variable manually

--- loaders/index.js
+++ loaders/index.js
@@ -1,6 +1,7 @@
 const expressLoader = require('./express');
 const passportLoader = require('./passport');
 const routeLoader = require('../routes');
+require('dotenv').config;
 
 module.exports = async (app) => {
   const expressApp = await expressLoader(app);
@@ -8,6 +9,8 @@ module.exports = async (app) => {
   const passport = await passportLoader(expressApp);
 
   await routeLoader(app, passport);
+
+  console.log('Database_URL', process.env.DATABASE_URL);
 
   app.use((err, req, res, next) => {
     const { message, status } = err;
